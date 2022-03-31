1550941456cf896b6d42a924aff2d28cb94d260a
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-31 17:47:16 -0300

    Add repositories for Aluno and Avaliacao

---
+++ src/main/java/br/com/zup/handora/muitosparaum3/repositories/AlunoRepository.java
@@ -0,0 +1,9 @@
+package br.com.zup.handora.muitosparaum3.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.zup.handora.muitosparaum3.models.Aluno;
+
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+
+}
---
+++ src/main/java/br/com/zup/handora/muitosparaum3/repositories/AvaliacaoRepository.java
@@ -0,0 +1,9 @@
+package br.com.zup.handora.muitosparaum3.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.zup.handora.muitosparaum3.models.Avaliacao;
+
+public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Long> {
+
+}
