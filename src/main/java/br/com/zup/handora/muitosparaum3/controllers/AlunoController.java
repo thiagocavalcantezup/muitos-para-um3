1f0d2296e1945ddda23060fc2c6bae2e42df6425
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-31 19:12:50 -0300

    Add creation methods in controllers

--- src/main/java/br/com/zup/handora/muitosparaum3/controllers/AlunoController.java
+++ src/main/java/br/com/zup/handora/muitosparaum3/controllers/AlunoController.java
@@ -1,8 +1,18 @@
 package br.com.zup.handora.muitosparaum3.controllers;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.zup.handora.muitosparaum3.models.Aluno;
+import br.com.zup.handora.muitosparaum3.models.AlunoDTO;
 import br.com.zup.handora.muitosparaum3.repositories.AlunoRepository;
 
 @RestController
@@ -15,6 +25,18 @@ public class AlunoController {
 
     public AlunoController(AlunoRepository alunoRepository) {
         this.alunoRepository = alunoRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody @Valid AlunoDTO alunoDTO,
+                                       UriComponentsBuilder uriComponentsBuilder) {
+        Aluno aluno = alunoRepository.save(alunoDTO.paraAluno());
+
+        URI location = uriComponentsBuilder.path(BASE_URI + "/{id}")
+                                           .buildAndExpand(aluno.getId())
+                                           .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 }
--- src/main/java/br/com/zup/handora/muitosparaum3/controllers/AvaliacaoController.java
+++ src/main/java/br/com/zup/handora/muitosparaum3/controllers/AvaliacaoController.java
@@ -1,8 +1,22 @@
 package br.com.zup.handora.muitosparaum3.controllers;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.zup.handora.muitosparaum3.models.Aluno;
+import br.com.zup.handora.muitosparaum3.models.Avaliacao;
+import br.com.zup.handora.muitosparaum3.models.AvaliacaoDTO;
 import br.com.zup.handora.muitosparaum3.repositories.AlunoRepository;
 import br.com.zup.handora.muitosparaum3.repositories.AvaliacaoRepository;
 
@@ -19,6 +33,26 @@ public class AvaliacaoController {
                                AvaliacaoRepository avaliacaoRepository) {
         this.alunoRepository = alunoRepository;
         this.avaliacaoRepository = avaliacaoRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create(@PathVariable Long alunoId,
+                                       @RequestBody @Valid AvaliacaoDTO avaliacaoDTO,
+                                       UriComponentsBuilder uriComponentsBuilder) {
+        Aluno aluno = alunoRepository.findById(alunoId)
+                                     .orElseThrow(
+                                         () -> new ResponseStatusException(
+                                             HttpStatus.NOT_FOUND,
+                                             "Não existe um aluno com o ID fornecido."
+                                         )
+                                     );
+        Avaliacao avaliacao = avaliacaoRepository.save(avaliacaoDTO.paraAvaliacao(aluno));
+
+        URI location = uriComponentsBuilder.path(
+            AlunoController.BASE_URI + "/{alunoId}" + BASE_URI + "/{id}"
+        ).buildAndExpand(aluno.getId(), avaliacao.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 }
