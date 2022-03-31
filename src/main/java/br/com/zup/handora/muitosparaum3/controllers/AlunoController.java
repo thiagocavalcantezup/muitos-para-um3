4907bdf06d9f1ba279a7bc4b97d48ee67f949cec
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-31 19:03:47 -0300

    Add repository dependencies to controllers

--- src/main/java/br/com/zup/handora/muitosparaum3/controllers/AlunoController.java
+++ src/main/java/br/com/zup/handora/muitosparaum3/controllers/AlunoController.java
@@ -3,10 +3,18 @@ package br.com.zup.handora.muitosparaum3.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.zup.handora.muitosparaum3.repositories.AlunoRepository;
+
 @RestController
 @RequestMapping(AlunoController.BASE_URI)
 public class AlunoController {
 
     public final static String BASE_URI = "/alunos";
+
+    private final AlunoRepository alunoRepository;
+
+    public AlunoController(AlunoRepository alunoRepository) {
+        this.alunoRepository = alunoRepository;
+    }
 
 }
--- src/main/java/br/com/zup/handora/muitosparaum3/controllers/AvaliacaoController.java
+++ src/main/java/br/com/zup/handora/muitosparaum3/controllers/AvaliacaoController.java
@@ -3,10 +3,22 @@ package br.com.zup.handora.muitosparaum3.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.zup.handora.muitosparaum3.repositories.AlunoRepository;
+import br.com.zup.handora.muitosparaum3.repositories.AvaliacaoRepository;
+
 @RestController
 @RequestMapping(AlunoController.BASE_URI + "/{alunoId}" + AvaliacaoController.BASE_URI)
 public class AvaliacaoController {
 
     public final static String BASE_URI = "/avaliacoes";
+
+    private final AlunoRepository alunoRepository;
+    private final AvaliacaoRepository avaliacaoRepository;
+
+    public AvaliacaoController(AlunoRepository alunoRepository,
+                               AvaliacaoRepository avaliacaoRepository) {
+        this.alunoRepository = alunoRepository;
+        this.avaliacaoRepository = avaliacaoRepository;
+    }
 
 }
